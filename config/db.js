f1642de4822297ffb4e76f1d0b520c40dcec4c3f
Scott Snodgrass <github@ssnodgrass.com>
2020-10-08 10:24:05 -0400

    Colors in the Console

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,
   });
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
