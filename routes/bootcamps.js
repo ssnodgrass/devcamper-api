d1a711c65303300f2c371a963491c68be1a4e1a7
Scott Snodgrass <github@ssnodgrass.com>
2020-10-21 13:07:57 -0400

    Photo Upload for Bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,6 +6,7 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
 // Include other resource routers
@@ -25,5 +26,7 @@ router
   .delete(deleteBootcamp);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload)
 
 module.exports = router;
