16e0eb0dbefe874dfbb1a33706ee8ce64dfe9aff
Scott Snodgrass <github@ssnodgrass.com>
2020-10-21 15:59:23 -0400

    User login

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
-const bycrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose');
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name']
+    required: [true, 'Please add a name'],
   },
   email: {
     type: String,
@@ -19,33 +19,38 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['user', 'publisher'],
-    default: 'user'
+    default: 'user',
   },
   password: {
     type: String,
     required: [true, 'Please add a password'],
     minlength: 6,
-    select: false
+    select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   createdAt: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
 // Encrypt password using bcrypt
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function (next) {
   const salt = await bycrypt.genSalt(10);
-  this.password = await bycrypt.hash(this.password, salt)
-})
+  this.password = await bycrypt.hash(this.password, salt);
+});
 
 // Sign JWT and return
-UserSchema.methods.getSignedJwtToken = function() {
+UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
-  })
-}
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
-module.exports = mongoose.model('User', UserSchema)
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bycrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
