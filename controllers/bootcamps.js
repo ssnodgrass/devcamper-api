b756c47787cf6f7229a53c082e2a2406959d850e
Scott Snodgrass <github@ssnodgrass.com>
2020-10-22 15:27:08 -0400

    Bootcamp & User Relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
@@ -8,9 +8,7 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  res
-    .status(200)
-    .json(res.advancedResults);
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get single bootcamp
@@ -33,6 +31,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // if the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
@@ -115,42 +129,41 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     );
   }
 
-  if(!req.files) {
-    return next(
-      new ErrorResponse(`Please upload a file`, 400)
-    );
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
   }
 
   const file = req.files.file;
 
   // Make sure the image is a photo
-  if(!file.mimetype.startsWith('image')) {
-    return next(
-      new ErrorResponse(`Please upload an image file`, 400)
-    );
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`Please upload an image file`, 400));
   }
 
   // Check filesize
-  if(file.size > process.env.MAX_FILE_UPLOAD) {
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
     return next(
-      new ErrorResponse(`Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 400)
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
     );
   }
 
   // Create custom filename
-  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
 
-  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
-    if(err) {
-      console.error(err)
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
       return next(new ErrorResponse(`Problem with file upload`, 500));
     }
 
-    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name })
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
 
     res.status(200).json({
       success: true,
-      data: file.name
-    })
-  })
+      data: file.name,
+    });
+  });
 });
