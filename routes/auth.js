61a9c425a519854efe57e9caf9700caf80263bfa
Scott Snodgrass <github@ssnodgrass.com>
2020-10-28 15:22:29 -0400

    Logout to clear token cookie

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -15,6 +16,7 @@ const { auth } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', auth, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
